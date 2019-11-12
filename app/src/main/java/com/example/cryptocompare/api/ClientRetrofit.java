87f769e4411ddeceefae34645bd78f0a3243808f
navjotrana28 <navjotrana28@gmail.com>
2019-11-12 13:07:43 +0530

    cryptocompare whole pack

--- app/src/main/java/com/example/cryptocompare/api/ClientRetrofit.java
+++ app/src/main/java/com/example/cryptocompare/api/ClientRetrofit.java
@@ -1,13 +1,18 @@
 package com.example.cryptocompare.api;
 
+import android.os.Build;
 import android.util.Log;
 
+import androidx.annotation.RequiresApi;
+
+import com.example.cryptocompare.apiCallAndResponse.Example;
+import com.example.cryptocompare.graphResponse.MyPojo;
 import com.example.cryptocompare.interfaces.GraphInterface;
 import com.example.cryptocompare.interfaces.InterfaceCallback;
 import com.example.cryptocompare.interfaces.NewsInterface;
-import com.example.cryptocompare.apiCallAndResponse.Example;
-import com.example.cryptocompare.graphResponse.MyPojo;
 import com.example.cryptocompare.newsResponse.NewsResult;
+
+import java.util.Objects;
 
 import io.reactivex.SingleObserver;
 import io.reactivex.android.schedulers.AndroidSchedulers;
@@ -19,93 +24,95 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ClientRetrofit {
 
-    private static final String BASE_URL = "https://min-api.cryptocompare.com";
-    private static final String API = "33354f1d432a0dcf2185b2dee2554f06ee3a2b1490c1e5e3b6958d1620e17445";
+	private static final String BASE_URL = "https://min-api.cryptocompare.com";
+	private static final String API = "33354f1d432a0dcf2185b2dee2554f06ee3a2b1490c1e5e3b6958d1620e17445";
 
-    private static Retrofit retrofit = null;
-    private static  ServiceRetrofit serviceRetrofit = null;
+	private static Retrofit retrofit = null;
+	private static ServiceRetrofit serviceRetrofit = null;
 
-    public ClientRetrofit() {
-        if (retrofit == null) {
-            retrofit = new Retrofit.Builder()
-                    .baseUrl(BASE_URL)
-                    .addConverterFactory(GsonConverterFactory.create())
-                    .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
-                    .build();
-        }
-        serviceRetrofit = retrofit.create(ServiceRetrofit.class);
-    }
+	public ClientRetrofit() {
+		if (retrofit == null) {
+			retrofit = new Retrofit.Builder()
+					.baseUrl(BASE_URL)
+					.addConverterFactory(GsonConverterFactory.create())
+					.addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+					.build();
+		}
+		serviceRetrofit = retrofit.create(ServiceRetrofit.class);
+	}
 /*
     public static void loadNewsData() {
     }*/
 
-    public static void loadJSON(final InterfaceCallback interfaceCallback) {
-        serviceRetrofit.getTopVolumeData(API)
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(new SingleObserver<Example>() {
-                    @Override
-                    public void onSubscribe(Disposable d) {
-                    }
+	public static void loadJSON(final InterfaceCallback interfaceCallback) {
+		serviceRetrofit.getTopVolumeData(API)
+				.subscribeOn(Schedulers.io())
+				.observeOn(AndroidSchedulers.mainThread())
+				.subscribe(new SingleObserver<Example>() {
+					@Override
+					public void onSubscribe(Disposable d) {
+					}
 
-                    @Override
-                    public void onSuccess(Example example) {
-                        interfaceCallback.onSuccess(example);
-                    }
+					@Override
+					public void onSuccess(Example example) {
+						interfaceCallback.onSuccess(example);
+					}
 
-                    @Override
-                    public void onError(Throwable e) {
+					@Override
+					public void onError(Throwable e) {
 
-                    }
-                });
-    }
+					}
+				});
+	}
 
-    public static void loadNewsData(final NewsInterface newsInterface) {
-        Log.d("result", "resultthere");
+	public static void loadNewsData(final NewsInterface newsInterface) {
+		Log.d("result", "resultthere");
 
-        serviceRetrofit.getNewsData(API)
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(new SingleObserver<NewsResult>() {
-                    @Override
-                    public void onSubscribe(Disposable d) {
-                    }
+		serviceRetrofit.getNewsData(API)
+				.subscribeOn(Schedulers.io())
+				.observeOn(AndroidSchedulers.mainThread())
+				.subscribe(new SingleObserver<NewsResult>() {
+					@Override
+					public void onSubscribe(Disposable d) {
+					}
 
-                    @Override
-                    public void onSuccess(NewsResult newsResult) {
-                        newsInterface.onSuccessNews(newsResult);
-                    }
+					@Override
+					public void onSuccess(NewsResult newsResult) {
+						newsInterface.onSuccessNews(newsResult);
+					}
 
-                    @Override
-                    public void onError(Throwable e) {
-                        Log.d("result", e.getLocalizedMessage());
+					@RequiresApi(api = Build.VERSION_CODES.KITKAT)
+					@Override
+					public void onError(Throwable e) {
+						Log.d("result", Objects.requireNonNull(e.getLocalizedMessage()));
 
-                    }
-                });
-    }
+					}
+				});
+	}
 
-    public void loadGraphData(String time,String coinSymbol,String coinUsd,int limit,final GraphInterface graphInterface) {
-        Log.d("graphresult", coinSymbol);
+	public void loadGraphData(String time, String coinSymbol, String coinUsd, int limit, final GraphInterface graphInterface) {
+		Log.d("graphresult", coinSymbol);
 
-        serviceRetrofit.getGraphData(time,coinSymbol,coinUsd,limit,API)
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(new SingleObserver<MyPojo>() {
-                    @Override
-                    public void onSubscribe(Disposable d) {
-                    }
+		serviceRetrofit.getGraphData(time, coinSymbol, coinUsd, limit, API)
+				.subscribeOn(Schedulers.io())
+				.observeOn(AndroidSchedulers.mainThread())
+				.subscribe(new SingleObserver<MyPojo>() {
+					@Override
+					public void onSubscribe(Disposable d) {
+					}
 
-                    @Override
-                    public void onSuccess(MyPojo myPojo) {
-                        Log.d("graphresultsuccess", "resultthere");
-                        graphInterface.onGraphSuccess(myPojo);
-                    }
+					@Override
+					public void onSuccess(MyPojo myPojo) {
+						Log.d("graphresultsuccess", "resultthere");
+						graphInterface.onGraphSuccess(myPojo);
+					}
 
-                    @Override
-                    public void onError(Throwable e) {
-                        Log.d("result", e.getLocalizedMessage());
+					@RequiresApi(api = Build.VERSION_CODES.KITKAT)
+					@Override
+					public void onError(Throwable e) {
+						Log.d("result", Objects.requireNonNull(e.getLocalizedMessage()));
 
-                    }
-                });
-    }
+					}
+				});
+	}
 }
